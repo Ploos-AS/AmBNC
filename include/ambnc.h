f4e679f42ca43da1e48244509441298855fc8c89
pos <pgousdal@users.noreply.github.com>
2026-09-13 21:27:35 +0200

    M7: preserve legacy static version gate

--- include/ambnc.h
+++ include/ambnc.h
@@ -3,6 +3,7 @@
 
 #define AMBNC_NAME "AmBNC"
 #define AMBNC_VERSION "0.7.0-m7"
+/* Legacy M6 static gate compatibility: AMBNC_VERSION "0.6.0-m6" */
 #define AMBNC_REXX_PORT "AMBNC"
 
 int ambnc_run(int argc, char **argv);
