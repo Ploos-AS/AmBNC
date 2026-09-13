a9182e1a646f9340ad3e208e141da060e43d5ec1
pos <pgousdal@users.noreply.github.com>
2026-09-13 19:25:46 +0200

    M1: add bsdsocket network API

---
+++ include/net.h
@@ -0,0 +1,11 @@
+#ifndef AMBNC_NET_H
+#define AMBNC_NET_H
+
+int ambnc_net_open(void);
+void ambnc_net_close(void);
+int ambnc_net_connect_ipv4(const char *host, unsigned short port);
+int ambnc_net_send_all(int sock, const char *data, unsigned int length);
+int ambnc_net_recv(int sock, char *buffer, unsigned int length);
+void ambnc_net_close_socket(int sock);
+
+#endif
