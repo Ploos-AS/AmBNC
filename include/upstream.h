e4af970225c63a479a90b62af3ebbcc1ca2dc740
pos <pgousdal@users.noreply.github.com>
2026-09-13 19:46:37 +0200

    M4: add runtime backlog limit to upstream config

--- include/upstream.h
+++ include/upstream.h
@@ -8,6 +8,7 @@ struct ambnc_upstream_config {
     const char *user;
     const char *pass;
     unsigned short listen_port;
+    unsigned int backlog_lines;
 };
 
 int ambnc_upstream_run(const struct ambnc_upstream_config *config);
