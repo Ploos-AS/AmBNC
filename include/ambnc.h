588e7ba208b620a22cd401ff043769f1c93d590d
pos <pgousdal@users.noreply.github.com>
2026-09-13 19:53:33 +0200

    M5: bump version

--- include/ambnc.h
+++ include/ambnc.h
@@ -2,7 +2,7 @@
 #define AMBNC_H
 
 #define AMBNC_NAME "AmBNC"
-#define AMBNC_VERSION "0.4.0-m4"
+#define AMBNC_VERSION "0.5.0-m5"
 #define AMBNC_REXX_PORT "AMBNC"
 
 int ambnc_run(int argc, char **argv);
