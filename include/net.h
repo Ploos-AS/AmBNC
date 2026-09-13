da24d2ea66ac017ff49a2cc56981362813388a7f
pos <pgousdal@users.noreply.github.com>
2026-09-13 19:36:30 +0200

    M2: extend network adapter for listener and multiplexing

--- include/net.h
+++ include/net.h
@@ -4,8 +4,15 @@
 int ambnc_net_open(void);
 void ambnc_net_close(void);
 int ambnc_net_connect_ipv4(const char *host, unsigned short port);
+int ambnc_net_listen_ipv4(unsigned short port);
+int ambnc_net_accept(int listener);
 int ambnc_net_send_all(int sock, const char *data, unsigned int length);
 int ambnc_net_recv(int sock, char *buffer, unsigned int length);
+int ambnc_net_wait_many(const int *socks,
+                        unsigned int count,
+                        unsigned long signal_mask,
+                        unsigned long *signals,
+                        unsigned long *ready_mask);
 void ambnc_net_close_socket(int sock);
 
 #endif
