93a0991d790797759bc659fde5e289b13faa358d
pos <pgousdal@users.noreply.github.com>
2026-09-13 19:46:01 +0200

    M4: extend state for timestamped backlog replay

--- include/state.h
+++ include/state.h
@@ -7,15 +7,19 @@
 #define AMBNC_STATE_NICK_MAX 31
 #define AMBNC_STATE_TARGET_MAX 63
 #define AMBNC_STATE_RING_TARGETS_MAX 16
-#define AMBNC_STATE_RING_LINES 32
+#define AMBNC_STATE_RING_LINES_MAX 32
+#define AMBNC_STATE_RING_LINES_DEFAULT 32
 
 struct ambnc_ring_line {
     char line[AMBNC_IRC_LINE_MAX + 1];
+    unsigned long days;
+    unsigned long minutes;
+    unsigned long ticks;
 };
 
 struct ambnc_target_ring {
     char target[AMBNC_STATE_TARGET_MAX + 1];
-    struct ambnc_ring_line lines[AMBNC_STATE_RING_LINES];
+    struct ambnc_ring_line lines[AMBNC_STATE_RING_LINES_MAX];
     unsigned int start;
     unsigned int count;
     unsigned long dropped;
@@ -27,11 +31,19 @@ struct ambnc_session_state {
     unsigned int channel_count;
     struct ambnc_target_ring rings[AMBNC_STATE_RING_TARGETS_MAX];
     unsigned int ring_count;
+    unsigned int ring_lines_limit;
 };
 
-void ambnc_state_init(struct ambnc_session_state *state, const char *nick);
+typedef int (*ambnc_state_send_line_fn)(void *userdata, const char *line);
+
+void ambnc_state_init(struct ambnc_session_state *state,
+                      const char *nick,
+                      unsigned int ring_lines_limit);
 void ambnc_state_observe_line(struct ambnc_session_state *state,
                               const char *line,
                               int downstream_attached);
+int ambnc_state_replay(struct ambnc_session_state *state,
+                       ambnc_state_send_line_fn send_line,
+                       void *userdata);
 
 #endif
